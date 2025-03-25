0333c8f86ec6d4ba187e28edf9b1ec20d07132d5
Zac-Digital <119302767+Zac-Digital@users.noreply.github.com>
2025-03-25 17:01:05 +0000

    feat: implement govuk frontend

--- ServiceDirectory.Presentation.Web/Program.cs
+++ ServiceDirectory.Presentation.Web/Program.cs
@@ -1,33 +1,29 @@
+using GovUk.Frontend.AspNetCore;
+
 namespace ServiceDirectory.Presentation.Web;
 
-public class Program
+public static class Program
 {
     public static void Main(string[] args)
     {
-        var builder = WebApplication.CreateBuilder(args);
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
         builder.Services.AddRazorPages();
+        builder.Services.AddGovUkFrontend();
 
-        var app = builder.Build();
+        WebApplication app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (!app.Environment.IsDevelopment())
         {
             app.UseExceptionHandler("/Error");
-            // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
             app.UseHsts();
         }
 
         app.UseHttpsRedirection();
-
         app.UseRouting();
-
         app.UseAuthorization();
-
         app.MapStaticAssets();
-        app.MapRazorPages()
-            .WithStaticAssets();
+        app.MapRazorPages().WithStaticAssets();
 
         app.Run();
     }
