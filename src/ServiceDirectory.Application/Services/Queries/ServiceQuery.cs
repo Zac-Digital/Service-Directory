b7c3dde908a271c52841ec1f94866ba6d1040f0e
Zac-Digital <119302767+Zac-Digital@users.noreply.github.com>
2025-06-05 22:13:08 +0100

    refactor: bounding box pre-filter for distance calc, more than 33x speedup

--- src/ServiceDirectory.Application/Services/Queries/ServiceQuery.cs
+++ src/ServiceDirectory.Application/Services/Queries/ServiceQuery.cs
@@ -18,6 +18,17 @@ public class ServiceQuery : IServiceQuery
         const double earthRadiusInMetres = 6378100.0d;
         const double degreesToRadians = Math.PI / 180.0d;
         const double metresToMiles = 0.000621371d;
+        const double metresPerRadian = 111320.0d;
+
+        const double twentyMilesInMetres = 32186.9d; // TODO: Temporary, will be capped by UI filters
+
+        const double latitudeDelta = twentyMilesInMetres / metresPerRadian; // TODO: Temporary, will be capped by UI filters
+        double longitudeDelta = twentyMilesInMetres / (111320.0 * Math.Cos(latitude * degreesToRadians));
+
+        double minLatitude = latitude - latitudeDelta;
+        double maxLatitude = latitude + latitudeDelta;
+        double minLongitude = longitude - longitudeDelta;
+        double maxLongitude = longitude + longitudeDelta;
 
         // Haversine Algorithm translated to LINQ
         // Credit to GitHub User https://github.com/hypersolutions
@@ -26,12 +37,14 @@ public class ServiceQuery : IServiceQuery
         (
             from s in _applicationDbContext.Services
             from l in s.Locations
+            where l.Latitude >= minLatitude && l.Latitude <= maxLatitude && l.Longitude >= minLongitude &&
+                  l.Longitude <= maxLongitude
             let d = 2 * earthRadiusInMetres * Math.Asin(
                 Math.Sqrt(Math.Pow(Math.Sin((latitude - l.Latitude) * degreesToRadians / 2), 2) +
                           Math.Cos(l.Latitude * degreesToRadians) *
                           Math.Cos(latitude * degreesToRadians) *
                           Math.Pow(Math.Sin((longitude - l.Longitude) * degreesToRadians / 2), 2)))
-            where d <= 32186.9 // TODO: Temporary, will be capped by UI filters
+            where d <= twentyMilesInMetres // TODO: Temporary, will be capped by UI filters
             orderby d
             select new Service
             {
@@ -43,9 +56,9 @@ public class ServiceQuery : IServiceQuery
                 Schedule = s.Schedule
             }
         );
-        
+
         int totalCount = serviceList.Count();
-        List<Service> result = serviceList.Skip((pageNumber - 1) * 10).Take(10).ToList();
+        IEnumerable<Service> result = serviceList.Skip((pageNumber - 1) * 10).Take(10);
 
         return new ServiceList(result, totalCount);
     }
