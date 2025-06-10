5e30aff3fa1cbe87d6ff4c6a40118c58d2c12f99
Zac-Digital <119302767+Zac-Digital@users.noreply.github.com>
2025-06-10 18:31:26 +0100

    refactor: revert changes

--- src/ServiceDirectory.Presentation.Web/Program.cs
+++ src/ServiceDirectory.Presentation.Web/Program.cs
@@ -8,72 +8,78 @@ using ServiceDirectory.Infrastructure.Data;
 using ServiceDirectory.Infrastructure.Postcode;
 using ServiceDirectory.Presentation.Web.Endpoints;
 
-WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+namespace ServiceDirectory.Presentation.Web;
 
-builder.Services.AddRazorPages();
-builder.Services.AddGovUkFrontend();
-
-builder.Services.AddHttpClient<IPostcodeClient, PostcodeClient>(client =>
+public class Program
 {
-    client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("ExternalServices:PostcodesIo")!);
-});
-
-builder.Services.AddTransient<IPostcodeQuery, PostcodeQuery>();
-builder.Services.AddTransient<IServiceQuery, ServiceQuery>();
-
-if (builder.Environment.IsDevelopment())
-{
-    builder.Services.AddEndpointsApiExplorer();
-    builder.Services.AddOpenApiDocument(options =>
+    public static async Task Main(string[] args)
     {
-        options.PostProcess = document =>
+        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
+
+        builder.Services.AddRazorPages();
+        builder.Services.AddGovUkFrontend();
+        
+        builder.Services.AddHttpClient<IPostcodeClient, PostcodeClient>(client =>
         {
-            document.Info = new OpenApiInfo
+            client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("ExternalServices:PostcodesIo")!);
+        });
+
+        builder.Services.AddTransient<IPostcodeQuery, PostcodeQuery>();
+        builder.Services.AddTransient<IServiceQuery, ServiceQuery>();
+
+        if (builder.Environment.IsDevelopment())
+        {
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddOpenApiDocument(options =>
             {
-                Version = "Version 1.0",
-                Title = "Service Directory - API",
-                Description = "The API component of the Service Directory Web Application"
-            };
-        };
-    });
+                options.PostProcess = document =>
+                {
+                    document.Info = new OpenApiInfo
+                    {
+                        Version = "Version 1.0",
+                        Title = "Service Directory - API",
+                        Description = "The API component of the Service Directory Web Application"
+                    };
+                };
+            });
+            
+            builder.Services.AddTransient<IMockDataCommand, MockDataCommand>();
+            builder.Services.AddSingleton<MinimalServiceEndpoints>();
+        }
 
-    builder.Services.AddTransient<IMockDataCommand, MockDataCommand>();
-    builder.Services.AddSingleton<MinimalServiceEndpoints>();
+        builder.Services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
+        {
+            options.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
+            options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTrackingWithIdentityResolution);
+        });
+
+        WebApplication app = builder.Build();
+
+        if (app.Environment.IsDevelopment())
+        {
+            using IServiceScope scope = app.Services.CreateScope();
+            
+            ApplicationDbContext applicationDbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+            await applicationDbContext.Database.MigrateAsync();
+            
+            scope.ServiceProvider.GetRequiredService<MinimalServiceEndpoints>().Register(app);
+            
+            app.UseOpenApi();
+            app.MapOpenApi();
+            app.UseSwaggerUi();
+        }
+        else
+        {
+            app.UseExceptionHandler("/Error");
+            app.UseHsts();
+        }
+
+        app.UseHttpsRedirection();
+
+        app.UseRouting();
+        app.MapStaticAssets();
+        app.MapRazorPages().WithStaticAssets();
+
+        await app.RunAsync();
+    }
 }
-
-builder.Services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
-{
-    options.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
-    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTrackingWithIdentityResolution);
-});
-
-WebApplication app = builder.Build();
-
-if (app.Environment.IsDevelopment())
-{
-    using IServiceScope scope = app.Services.CreateScope();
-
-    ApplicationDbContext applicationDbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-    await applicationDbContext.Database.MigrateAsync();
-
-    scope.ServiceProvider.GetRequiredService<MinimalServiceEndpoints>().Register(app);
-
-    app.UseOpenApi();
-    app.MapOpenApi();
-    app.UseSwaggerUi();
-}
-else
-{
-    app.UseExceptionHandler("/Error");
-    app.UseHsts();
-}
-
-app.UseHttpsRedirection();
-
-app.UseRouting();
-app.MapStaticAssets();
-app.MapRazorPages().WithStaticAssets();
-
-await app.RunAsync();
-
-public abstract partial class Program;
