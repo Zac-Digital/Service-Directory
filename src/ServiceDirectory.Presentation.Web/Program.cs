a0d155337fb28a7d3b444014178bfbbf9960c46f
Zac-Digital <119302767+Zac-Digital@users.noreply.github.com>
2025-06-10 18:06:38 +0100

    refactor: convert from top-level statements

--- src/ServiceDirectory.Presentation.Web/Program.cs
+++ src/ServiceDirectory.Presentation.Web/Program.cs
@@ -8,78 +8,70 @@ using ServiceDirectory.Infrastructure.Data;
 using ServiceDirectory.Infrastructure.Postcode;
 using ServiceDirectory.Presentation.Web.Endpoints;
 
-namespace ServiceDirectory.Presentation.Web;
+WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
 
-public class Program
+builder.Services.AddRazorPages();
+builder.Services.AddGovUkFrontend();
+
+builder.Services.AddHttpClient<IPostcodeClient, PostcodeClient>(client =>
 {
-    public static async Task Main(string[] args)
+    client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("ExternalServices:PostcodesIo")!);
+});
+
+builder.Services.AddTransient<IPostcodeQuery, PostcodeQuery>();
+builder.Services.AddTransient<IServiceQuery, ServiceQuery>();
+
+if (builder.Environment.IsDevelopment())
+{
+    builder.Services.AddEndpointsApiExplorer();
+    builder.Services.AddOpenApiDocument(options =>
     {
-        WebApplicationBuilder builder = WebApplication.CreateBuilder(args);
-
-        builder.Services.AddRazorPages();
-        builder.Services.AddGovUkFrontend();
-        
-        builder.Services.AddHttpClient<IPostcodeClient, PostcodeClient>(client =>
+        options.PostProcess = document =>
         {
-            client.BaseAddress = new Uri(builder.Configuration.GetValue<string>("ExternalServices:PostcodesIo")!);
-        });
-
-        builder.Services.AddTransient<IPostcodeQuery, PostcodeQuery>();
-        builder.Services.AddTransient<IServiceQuery, ServiceQuery>();
-
-        if (builder.Environment.IsDevelopment())
-        {
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddOpenApiDocument(options =>
+            document.Info = new OpenApiInfo
             {
-                options.PostProcess = document =>
-                {
-                    document.Info = new OpenApiInfo
-                    {
-                        Version = "Version 1.0",
-                        Title = "Service Directory - API",
-                        Description = "The API component of the Service Directory Web Application"
-                    };
-                };
-            });
-            
-            builder.Services.AddTransient<IMockDataCommand, MockDataCommand>();
-            builder.Services.AddSingleton<MinimalServiceEndpoints>();
-        }
+                Version = "Version 1.0",
+                Title = "Service Directory - API",
+                Description = "The API component of the Service Directory Web Application"
+            };
+        };
+    });
 
-        builder.Services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
-        {
-            options.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
-            options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTrackingWithIdentityResolution);
-        });
-
-        WebApplication app = builder.Build();
-
-        if (app.Environment.IsDevelopment())
-        {
-            using IServiceScope scope = app.Services.CreateScope();
-            
-            ApplicationDbContext applicationDbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
-            await applicationDbContext.Database.MigrateAsync();
-            
-            scope.ServiceProvider.GetRequiredService<MinimalServiceEndpoints>().Register(app);
-            
-            app.UseOpenApi();
-            app.MapOpenApi();
-            app.UseSwaggerUi();
-        }
-        else
-        {
-            app.UseExceptionHandler("/Error");
-            app.UseHsts();
-        }
-
-        app.UseHttpsRedirection();
-
-        app.UseRouting();
-        app.MapStaticAssets();
-        app.MapRazorPages().WithStaticAssets();
-
-        await app.RunAsync();
-    }
+    builder.Services.AddTransient<IMockDataCommand, MockDataCommand>();
+    builder.Services.AddSingleton<MinimalServiceEndpoints>();
 }
+
+builder.Services.AddDbContext<IApplicationDbContext, ApplicationDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
+    options.UseQueryTrackingBehavior(QueryTrackingBehavior.NoTrackingWithIdentityResolution);
+});
+
+WebApplication app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    using IServiceScope scope = app.Services.CreateScope();
+
+    ApplicationDbContext applicationDbContext = scope.ServiceProvider.GetRequiredService<ApplicationDbContext>();
+    await applicationDbContext.Database.MigrateAsync();
+
+    scope.ServiceProvider.GetRequiredService<MinimalServiceEndpoints>().Register(app);
+
+    app.UseOpenApi();
+    app.MapOpenApi();
+    app.UseSwaggerUi();
+}
+else
+{
+    app.UseExceptionHandler("/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+
+app.UseRouting();
+app.MapStaticAssets();
+app.MapRazorPages().WithStaticAssets();
+
+await app.RunAsync();
