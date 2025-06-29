1cae6f295238749de40212e1c95a50e36b960b06
Zac-Digital <119302767+Zac-Digital@users.noreply.github.com>
2025-06-29 14:46:22 +0100

    feat: add results filtering boilerplate

--- src/ServiceDirectory.Presentation.Web.Node/esbuild.config.js
+++ src/ServiceDirectory.Presentation.Web.Node/esbuild.config.js
@@ -14,28 +14,52 @@ await esbuild.build({
         "/assets/*"
     ],
     plugins: [
-        sassPlugin()
+        sassPlugin({
+            loadPaths: [
+                '.',
+                'node_modules/@ministryofjustice/frontend',
+                'node_modules/govuk-frontend/dist',
+            ],
+            quietDeps: true
+        })
     ],
     outfile: "out/css/application.css",
 });
 // -- Build CSS --
 
 // -- Copy GOV.UK Assets --
-const assetDirectory = "./node_modules/govuk-frontend/dist/govuk/assets/";
-const subFolders = ["images", "fonts"];
+const assetDirectoryGovUk = "./node_modules/govuk-frontend/dist/govuk/assets/";
+const subFoldersGovUk = ["images", "fonts"];
 
-for (const subFolder of subFolders) {
-    const targetDirectory = assetDirectory + subFolder;
+for (const subFolder of subFoldersGovUk) {
+    const targetDirectory = assetDirectoryGovUk + subFolder;
     readdirSync(targetDirectory).forEach(file => {
         cpSync(`${targetDirectory}/${file}`, `./out/assets/${subFolder}/${file}`, { force: true });
     });
 }
 // -- Copy GOV.UK Assets --
 
+// -- Copy MoJ Assets --
+const assetDirectoryMoJ = "./node_modules/@ministryofjustice/frontend/moj/assets/";
+const subFoldersMoJ = ["images"];
+
+for (const subFolder of subFoldersMoJ) {
+    const targetDirectory = assetDirectoryMoJ + subFolder;
+    readdirSync(targetDirectory).forEach(file => {
+        cpSync(`${targetDirectory}/${file}`, `./out/assets/${subFolder}/${file}`, { force: true });
+    });
+}
+// -- Copy MoJ Assets --
+
 // -- Copy GOV.UK Minified JS --
 cpSync("./node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js", "./out/js/govuk-frontend.min.js", { force: true });
 cpSync("./node_modules/govuk-frontend/dist/govuk/govuk-frontend.min.js.map", "./out/js/govuk-frontend.min.js.map", { force: true });
 // -- Copy GOV.UK Minified JS --
+
+// -- Copy MoJ Minified JS --
+cpSync("./node_modules/@ministryofjustice/frontend/moj/moj-frontend.min.js", "./out/js/moj-frontend.min.js", { force: true });
+cpSync("./node_modules/@ministryofjustice/frontend/moj/moj-frontend.min.js.map", "./out/js/moj-frontend.min.js.map", { force: true });
+// -- Copy MoJ Minified JS --
 
 // - Copy Output to ServiceDirectory.Presentation.Web --
 const outputDirectory = "../ServiceDirectory.Presentation.Web/wwwroot/";
